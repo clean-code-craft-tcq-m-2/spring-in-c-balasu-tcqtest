e138c7a761ba156290e034c0f45102606e9a77cf
balasu-tcqtest <115686515+balasu-tcqtest@users.noreply.github.com>
2022-10-14 12:19:35 +0530

    Corrected the Stats structure

--- stats.h
+++ stats.h
@@ -1,14 +1,14 @@
-struct Stats
+typedef struct
 {
   int min;
   int max;
   int average;
-};
+}Stats;
 
-struct Stats compute_statistics(const float* numberset, int setlength);
+Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], struct Stats computedStats);
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
