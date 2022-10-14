474347d4c9f560125a1d35f7218262f15d9bb2e5
balasu-tcqtest <115686515+balasu-tcqtest@users.noreply.github.com>
2022-10-14 13:51:07 +0530

    Completed function compute_statistics

--- stats.c
+++ stats.c
@@ -1,10 +1,37 @@
 #include "stats.h"
 
-struct Stats compute_statistics(const float* numberset, int setlength) {
+Stats compute_statistics(const float* numberset, int setlength) {
     struct Stats s;
-    s.average = 0;
-    s.min = 0;
-    s.max = 0;
+    s.average = 0.0;
+    s.min = 0.0;
+    s.max = 0.0;
+    float tempMin = 0.0;
+    float tempMax = 0.0;
+    float sum = 0.0;
+    int tempLen = 0;
+    
+    while(tempLen < setlength)
+    {
+        if( numberset[tempLen]  > tempMax)
+        {
+            tempMax = numberset[tempLen];
+        }
+        else if( numberset[tempLen] < tempMin)
+        {
+            tempMin = numberset[tempLen];
+        }
+        else
+        {
+            // Do nothing
+        }
+        sum += numberset[tempLen];
+        tempLen++;
+    }
+    s.min = tempMin;
+    s.max = tempMax;
+    s.average = sum / setlength;
+    
+    return s;
 }
 
 int emailAlertCallCount = 0;
