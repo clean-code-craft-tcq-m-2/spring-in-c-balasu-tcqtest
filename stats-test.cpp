e02356d86d8b668ada972ff135c91c45b774adad
balasu-tcqtest <115686515+balasu-tcqtest@users.noreply.github.com>
2022-10-14 14:31:59 +0530

    Added debug code

--- stats-test.cpp
+++ stats-test.cpp
@@ -11,6 +11,7 @@ TEST_CASE("reports average, minimum and maximum") {
     int setlength = sizeof(numberset) / sizeof(numberset[0]);
     Stats computedStats = compute_statistics(numberset, setlength);
     float epsilon = 0.001;
+    printf("Min:%f,Max:%f,Avg:%f\n",computedStats.min,computedStats.max,computedStats.average);
     REQUIRE(abs(computedStats.average - 4.525) < epsilon);
     REQUIRE(abs(computedStats.max - 8.9) < epsilon);
     REQUIRE(abs(computedStats.min - 1.5) < epsilon);
