53bebd10dc734cb453ed8fa3259382b5d1e23596
balasu-tcqtest <115686515+balasu-tcqtest@users.noreply.github.com>
2022-10-14 15:00:55 +0530

    Update stats.h
    Updated check_and_alert function

--- stats.h
+++ stats.h
@@ -8,7 +8,7 @@ typedef struct
 Stats compute_statistics(const float* numberset, int setlength);
 
 typedef void (*alerter_funcptr)();
-void check_and_alert(float maxThreshold, alerter_funcptr alerters[], Stats computedStats);
+void check_and_alert(float maxThreshold, alerter_funcptr alerters[], int alertCount, Stats computedStats);
 
 extern int emailAlertCallCount;
 extern int ledAlertCallCount;
